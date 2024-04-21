cc373f27ed63bc5163370d38452c64dd5ad22b38
mushirarashid <mushirarashid2003@gmail.com>
2024-04-21 22:10:37 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,89 @@
+var a = 50;
+var b = 20;
+
+var sum = a - b;
+console.log(sum);
+
+var c = 23;
+var d = 5;
+
+var sum_2 = c % d;
+console.log(sum_2);
+var sam_3 = c / d;
+console.log(sam_3);
+
+// set a new
+
+var xyz = 100;
+console.log(xyz);
+
+xyz = xyz + 50;
+console.log(xyz);
+
+xyz += 70;
+console.log(xyz);
+
+var abc = "Musu";
+var acb = "Rashid";
+var fullName = abc + " " + acb;
+console.log(fullName);
+
+var axy = "250";
+console.log(axy);
+var acx = parseFloat(axy);
+console.log(acx);
+
+var sm = 0.1;
+var am = 0.2;
+
+var sum = sm + am;
+console.log(sum);
+
+var fixSum = sum.toFixed(3);
+console.log(fixSum);
+var convertToNumber = parseFloat(fixSum);
+console.log(convertToNumber);
+
+//  probling solving ===============
+
+// one
+
+var motherGive = 850;
+var apple = 200;
+var orange = 350;
+
+var expence = apple + orange;
+console.log(expence);
+
+var shopkeeperReturn = motherGive - expence;
+console.log(shopkeeperReturn);
+
+// Two
+
+var mathematic = 72;
+var biology = 60;
+var chemistry = 80;
+var physics = 85;
+var bangla = 79;
+
+var subSum = mathematic + biology + chemistry + physics + bangla;
+
+console.log(subSum);
+
+var average = subSum / 5;
+console.log(average);
+
+// Three
+
+var one = "I am going to be an awesome web developer.";
+
+var two = "Very soon I'm fulfill my dream in-sha-Allah.";
+
+var addTwo = one + " " + two;
+console.log(addTwo);
+
+// Four
+
+var mathOne = 119;
+var remainderSum = mathOne % 5;
+console.log(remainderSum);
